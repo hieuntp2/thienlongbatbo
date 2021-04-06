ed3cac49680e1452548b9966f2616fd7e837279d
Le Van Hieu <lehieusoftware@gmail.com>
2021-04-06 22:20:26 +0700

    updat home

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 function openMenuPart2(event, menuName) {
   let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("list-content");
@@ -10,7 +12,9 @@ function openMenuPart2(event, menuName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(menuName).className = document.getElementById(menuName).className.replace(" d__none", "");
+  document.getElementById(menuName).className = document
+    .getElementById(menuName)
+    .className.replace(" d__none", "");
   event.currentTarget.className += " active";
 }
 
@@ -26,7 +30,9 @@ function openMenuPart3BangPhai(event, menuName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(menuName).className = document.getElementById(menuName).className.replace(" d__none", "");
+  document.getElementById(menuName).className = document
+    .getElementById(menuName)
+    .className.replace(" d__none", "");
   event.currentTarget.className += " active";
 }
 
@@ -42,7 +48,9 @@ function openMenuPart3(event, menuName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(menuName).className = document.getElementById(menuName).className.replace(" d__none", "");
+  document.getElementById(menuName).className = document
+    .getElementById(menuName)
+    .className.replace(" d__none", "");
   event.currentTarget.className += " active";
 }
 
@@ -54,7 +62,9 @@ function openMenuWanfaPart3(menuName) {
       tabcontent[i].className += " d__none";
     }
   }
-  document.getElementById(menuName).className = document.getElementById(menuName).className.replace(" d__none", "");
+  document.getElementById(menuName).className = document
+    .getElementById(menuName)
+    .className.replace(" d__none", "");
 }
 
 function closeAllMenuWanfaPart3() {
@@ -67,3 +77,63 @@ function closeAllMenuWanfaPart3() {
   }
 }
 
+$(document).ready(function () {
+  $(".download__section").hide();
+  $(".righ-side-bar").hide();
+});
+
+window.onscroll = function () {
+  checkScrollEvent();
+};
+
+function checkScrollEvent() {
+  var height =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+  // console.log(height);
+
+  var body = document.body,
+    html = document.documentElement;
+
+  var windowHeight = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+
+  var heighPercent = height / windowHeight; // > 10% will show slider
+  console.log(heighPercent);
+
+  const topPercent = 0.1;
+  const bottomPercent = 0.7;
+  if (heighPercent < topPercent) {
+    onTopEvent();
+  } else if (heighPercent >= bottomPercent) {
+    onBottomEvent();
+  } else {
+    onMiddleEvent();
+  }
+
+  // if (heighPercent >= topPercent && bottomPercent < bottomPercent) {
+  //   onMiddleEvent();
+  // }
+}
+
+function onTopEvent() {
+  $(".download__section").hide();
+  $(".righ-side-bar").hide();
+}
+
+function onBottomEvent() {
+  $(".download__section").hide();
+  $(".righ-side-bar").hide();
+}
+
+function onMiddleEvent() {
+  $(".download__section").show();
+  $(".righ-side-bar").show();
+}
