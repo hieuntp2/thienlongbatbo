a2a4c2ced2ae8ef30a9d050b842267e8c159fef5
Nguyen Xuan Huan <huannx@dgroup.co>
2021-04-14 22:38:28 +0700

    update ui

--- js/index.js
+++ js/index.js
@@ -135,3 +135,44 @@ function onMiddleEvent() {
   $(".download__section").show();
   $(".righ-side-bar").show();
 }
+
+$(document).ready(function () {
+
+  // Gets the video src from the data-src on each button
+
+  var $videoSrc;
+  $('.video-btn').click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+
+
+
+  // when the modal is opened autoplay it  
+  $('#myModal').on('shown.bs.modal', function (e) {
+
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+  })
+
+
+
+  // stop playing the youtube video when I close the modal
+  $('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src', $videoSrc);
+  });
+
+  $(".toggle").click(function () {
+    if ($(".toggle").hasClass('toggle-close')) {
+      $(".toggle").removeClass('toggle-close');
+      $(".toggle").addClass('toggle-open');
+      $(".righ-side-bar").css("right", "-160px")
+    } else {
+      $(".toggle").removeClass('toggle-open');
+      $(".toggle").addClass('toggle-close');
+      $(".righ-side-bar").css("right", "0")
+    }
+  });
+});
+
